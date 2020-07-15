1f16b261bc44eec56fe3d247412391df8b749d3b
Matt Park <mattpark102@gmail.com>
2020-07-15 15:22:06 -0400

    Added user name to Transactions view

--- DataAccessLibrary/Models/User.cs
+++ DataAccessLibrary/Models/User.cs
@@ -3,6 +3,7 @@ namespace DataAccessLibrary.Models
     public class User
     {
         public string Id { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int GroupId { get; set; }
